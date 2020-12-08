95d6fca9fcc5cc83e20fe31d9c90da71997c1e59
Donnie Pollitz <chaos2007@gmail.com>
2020-12-09 00:41:47 +0100

    Rough part 2 day 3

--- day_03/test.cpp
+++ day_03/test.cpp
@@ -7,14 +7,17 @@ int get_num_trees_hit(std::string file_name, int right, int down) {
   std::ifstream file(file_name);
   std::string line;
   int current_column = 0;
+  int current_row = 0;
   int num_trees_hit = 0;
   while (std::getline(file, line)) {
-    if (down == 1) {
+    if (current_row == 0) {
       if (line[current_column] == '#') {
         num_trees_hit++;
       }
       current_column = (current_column + right) % line.size();
     }
+    current_row++;
+    if(current_row == down) current_row = 0;
   }
   return num_trees_hit;
 }
@@ -24,4 +27,20 @@ TEST(Day03, sample) {
 }
 TEST(Day03, part_01) {
   EXPECT_EQ(get_num_trees_hit("day03_part01.txt", 3, 1), 278);
+}
+TEST(Day03, variable_slopes) {
+  EXPECT_EQ(get_num_trees_hit("day03_sample.txt", 1, 1), 2);
+  EXPECT_EQ(get_num_trees_hit("day03_sample.txt", 3, 1), 7);
+  EXPECT_EQ(get_num_trees_hit("day03_sample.txt", 5, 1), 3);
+  EXPECT_EQ(get_num_trees_hit("day03_sample.txt", 7, 1), 4);
+  EXPECT_EQ(get_num_trees_hit("day03_sample.txt", 1, 2), 2);
+}
+TEST(Day03, part_02) {
+  long int multiply = 1;
+  multiply *= get_num_trees_hit("day03_part01.txt", 1, 1);
+  multiply *= get_num_trees_hit("day03_part01.txt", 3, 1);
+  multiply *= get_num_trees_hit("day03_part01.txt", 5, 1);
+  multiply *= get_num_trees_hit("day03_part01.txt", 7, 1);
+  multiply *= get_num_trees_hit("day03_part01.txt", 1, 2);
+  EXPECT_EQ(multiply, 9709761600);
 }
